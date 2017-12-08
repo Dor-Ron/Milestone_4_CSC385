afa459c5f3ab8e6d407a1f113cfebedc4507fe13
bdruschel <brandon.druschel@gmail.com>
2017-12-08 17:35:09 -0500

    Create AppTestSuite.java

---
+++ src/test/java/oswego/csc385/m4/AppTestSuite.java
@@ -0,0 +1,13 @@
+package oswego.csc385.m4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Brandon
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({oswego.csc385.m4.AppTest.class})
+public class AppTestSuite {
+}
